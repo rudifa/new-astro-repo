96efc50bc7c68be902916c482e11874dbbddf3dd
Olivier Lange <olange@hey.com>
2023-09-14 17:29:07 +0200

    Adds Vercel adapter for Astro build

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import vercel from '@astrojs/vercel/static';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  output: 'static',
+  adapter: vercel()
+});
