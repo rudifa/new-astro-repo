17b1f816f64db83135f89bab8a5b7b45f664eccd
Olivier Lange <olange@hey.com>
2023-09-19 18:34:22 +0200

    Adds Lit SSR integration for Astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 import { defineConfig } from 'astro/config';
-
 import vercel from '@astrojs/vercel/static';
+
+import lit from "@astrojs/lit";
 
 // https://astro.build/config
 export default defineConfig({
   output: 'static',
-  adapter: vercel()
+  adapter: vercel(),
+  integrations: [lit()]
 });
