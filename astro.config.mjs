f0f36139cbff2d34074265d1dd9c8194004ec36e
Olivier Lange <olange@hey.com>
2023-11-16 16:20:31 +0100

    Removes Astro Lit SSR integration
    (which blocks updating Lit from v2.8 to latest 3.1)

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,9 @@
 import { defineConfig } from 'astro/config';
 import vercel from '@astrojs/vercel/static';
 
-import lit from "@astrojs/lit";
-
 // https://astro.build/config
 export default defineConfig({
   output: 'static',
   adapter: vercel(),
-  integrations: [lit()]
+  integrations: []
 });
