5c0fb710be4c4882e9fca7c2d7ef3ba359544d82
Olivier Lange <olange@hey.com>
2024-10-08 21:57:09 +0200

    build: updates eslint-plugin-astro from v0.33.1 to latest 1.2.4

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,12 @@
 import eslint from "@eslint/js";
 import tseslint from 'typescript-eslint';
+import eslintPluginAstro from 'eslint-plugin-astro';
 // standard could not be migrated to ESLint v9, have a look at neostandard later
 
 export default [
   eslint.configs.recommended,
   ...tseslint.configs.recommended,
+  ...eslintPluginAstro.configs.recommended,
   {
     rules: {
       // override/add rules settings here, such as:
